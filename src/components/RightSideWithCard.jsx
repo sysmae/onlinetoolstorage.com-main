dfef4e7183b84eb56874fe857444676ddec05443
sysmae <j970330@gmail.com>
2024-08-10 22:29:24 +0900

    카피라이팅 완

--- src/components/RightSideWithCard.jsx
+++ src/components/RightSideWithCard.jsx
@@ -2,7 +2,7 @@ import { Flex } from '@radix-ui/themes'
 import SearchComponent from '@/components/SearchComponent'
 import { PopularCard } from '@/components/cards/PopularCard'
 import { RecentCommentsCard } from '@/components/cards/RecentCommentsCard'
-import { NewsletterCard } from '@/components/cards/NewsletterCard'
+// import { NewsletterCard } from '@/components/cards/NewsletterCard'
 
 import ResponsiveAd from '@/components/adsense/ResponsiveAd'
 import { RightSideWithCard_dataAdSlot } from '@/constants/adsense/data_ad_slot'
@@ -28,13 +28,13 @@ export default function RightSideWithCard() {
           data_ad_slot={RightSideWithCard_dataAdSlot[1].dataAdSlot}
         />
         <div className="pb-[30px]"></div>
-
+        {/* 
         <NewsletterCard />
-        <div className="pb-[30px]"></div>
+        <div className="pb-[30px]"></div> */}
 
-        <ResponsiveAd
+        {/* <ResponsiveAd
           data_ad_slot={RightSideWithCard_dataAdSlot[2].dataAdSlot}
-        />
+        /> */}
       </aside>
     </Flex>
   )
--- src/pages/business/qrcode.jsx
+++ src/pages/business/qrcode.jsx
@@ -36,7 +36,7 @@ export default function Home() {
       <CustomSEOContent category={category} />
       <CustomH1Content category={category} />
       <div className="container mx-auto p-4">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center xl:pt-12">
           <Input
             type="text"
             placeholder="Enter text to generate QR code"
